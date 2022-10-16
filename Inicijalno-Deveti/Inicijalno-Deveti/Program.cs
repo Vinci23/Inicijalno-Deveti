74218be928158bfd737c31dd61fdeff0950c6bef
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-10-16 23:46:50 +0200

    Add files via upload

---
+++ Inicijalno-Deveti/Inicijalno-Deveti/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_Deveti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>() { 4, 7, 45, 3, 11, 25, 1 };
+            lista = lista.OrderBy(i=>i).ToList();
+
+            for (int i=0; i<lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
